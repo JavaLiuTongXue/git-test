9035c19775ba4ea12edaea4aedab2794de83ae86
admin <admin@163.com>
2018-10-15 15:28:12 +0800

    修改Git-Test工程2

--- Git-Test/src/cn/itheima/GitTest.java
+++ Git-Test/src/cn/itheima/GitTest.java
@@ -3,6 +3,7 @@ package cn.itheima;
 public class GitTest {
     public  void teset(){
         int  a=10;
+        int b=100;
     }
     public static void main(String[] args) {
         System.out.println("Hello Word");
