13bd36d6c215ff31d6c01f44e0e51b7174bfd9ce
admin <admin@163.com>
2018-10-15 15:34:44 +0800

    git-test修改Git-Test工程3

--- Git-Test/src/cn/itheima/GitTest.java
+++ Git-Test/src/cn/itheima/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public  void teset(){
         int  a=10;
         int b=100;
+        System.out.println("你好,世界");
     }
     public static void main(String[] args) {
         System.out.println("Hello Word");
